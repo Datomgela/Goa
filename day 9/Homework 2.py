2d667ef87067c11ef1563c61c536bcd6d7e71f4c
Datomgela <138392124+Datomgela@users.noreply.github.com>
2023-09-26 16:25:24 +0400

    Create Homework 2.py

---
+++ day 9/Homework 2.py
@@ -0,0 +1,7 @@
+user = int(input("Enter your age: "))
+while user > 0:
+        print("Congratulations your age is ", str(user))
+        user = user - 1
+        while user == 8:
+                print("Congratulations your age is " + str(user) + " Congratulations now your are member in Goal-oriented-academy" )
+                user = user - 1
