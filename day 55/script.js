ebb3e9b8ba5ff7f37f0e3a94fdae14184af200e2
Datomgela <138392124+Datomgela@users.noreply.github.com>
2024-01-26 14:50:40 +0400

    Add files via upload

---
+++ day 55/script.js
@@ -0,0 +1,14 @@
+console.log(true || false);
+console.log(true && false);
+console.log(false || false);
+console.log(true && true);
+
+let x = 15;
+
+console.log(15 + String(15));
+
+let num1 = Number(prompt("Enter first number"));
+
+let num2 = Number(prompt("Enter second number"));
+
+console.log(num1 + num2);
