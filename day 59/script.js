e7b7737e79e2faee92e8e1ba4fb4bbd10801741d
Datomgela <138392124+Datomgela@users.noreply.github.com>
2024-02-14 17:57:23 +0400

    Add files via upload

---
+++ day 59/script.js
@@ -0,0 +1,26 @@
+let num = 1
+
+while(true){
+    if(20 === num){
+        break
+    }
+    console.log(num);
+    num++
+}
+
+let num1 = 5
+
+while (num1 <= 100){
+    console.log(num1)
+    num1++
+}
+
+let num2 = 15;
+
+if (15 > num2){
+    console.log("You are over 15 years old")
+}else if (15 < num2){
+    console.log("You are not over 15 years old")
+}else {
+    console.log("you are 15 years old")
+}
